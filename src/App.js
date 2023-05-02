94fd9baeba2290deb50ca7d51ed9a74926e502ff
burak.gul <Burak.Gul@payten.com>
2023-05-03 02:36:09 +0300

    "First Look"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navi from "./Navi";
+import {Container , Row} from "reactstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Container>
+      <Row>
+      <Navi></Navi>
+      </Row>
+      <Row>
+        <h1>Blog will be there!!!</h1>
+      </Row>
+     </Container>
     </div>
   );
 }
