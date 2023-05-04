b7c8108d3a1f019aa31547084a5b479bd030b9ea
burak.gul <Burak.Gul@payten.com>
2023-05-04 23:15:23 +0300

    add some pages

--- src/Navi.js
+++ src/Navi.js
@@ -4,7 +4,11 @@ export default class Navi extends Component {
   render() {
     return (
       <div>
-        <h3>Navi will be here!!</h3>
+        <h3>Home</h3>
+        <h3>Blog</h3>
+        <h3>Gallery</h3>
+        <h3>About</h3>
+        <h3>Contact</h3>
       </div>
     )
   }
